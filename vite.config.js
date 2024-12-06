e312c27e578e07a1017fdfbc72bacd20676e277a
Asora7 <verstr01474@stud.noroff.no>
2024-12-06 17:59:07 +0100

    Update Vite config for GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Set the base URL for GitHub Pages deployment
+  base: '/semester-project-2/',  // Replace with your GitHub repository name
+
+  build: {
+    outDir: 'dist',  // Output directory for your build files
+  },
+});
